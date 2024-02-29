b39486947c1947327388dfda3f1f9eb5673e5ffb
kevDevops <kelvinnyagaka7@gmail.com>
2024-02-29 05:49:43 -0800

    python user input

---
+++ portfoli/user_input.py
@@ -0,0 +1,5 @@
+#asking user details
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+print(f"Hello {name}, you are {age} years old and live in {location}.")
